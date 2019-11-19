306d611edfa19ef90820cb9e13da4b836faeb430
orcafang <orcafang@163.com>
2019-11-19 14:06:14 +0800

    mm11月19日白天学习提交

---
+++ absex1.py
@@ -0,0 +1,10 @@
+# This program says hello and ask for my name
+print("Hello World!")
+print('What is your name?') # ask for their name
+myName = input()
+print('It\'s good to meet you,' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What\'s your age?') # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
